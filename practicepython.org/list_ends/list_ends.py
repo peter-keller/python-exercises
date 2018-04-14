015bae16098d05285f609fcddb9ec89a7114f07f
peter-keller <keller.f.peter@gmail.com>
2018-04-14 22:26:36 +0100

    feat(list_ends): exercise finished

---
+++ practicepython.org/list_ends/list_ends.py
@@ -0,0 +1,13 @@
+# Write a program that takes a list of numbers (for example,
+# a = [5, 10, 15, 20, 25]) and makes a new list of only the
+# first and last elements of the given list. For practice,
+# write this code inside a function.
+
+a = [5, 10, 15, 20, 25]
+
+
+def list_numbers(x):
+    return [x[0], x[-1]]
+
+
+print(list_numbers(a))
